182355368a267a54db1b5114aff046b11bd5656f
Overowser <82826789+Overowser@users.noreply.github.com>
2024-12-01 07:23:15 +0100

    Update main.py
    - Added part 2 and 3 of the report
    - Changed the report language to french to adapt to moroccan context
    - Use of Groq api instead of local llm
    - Use of llama 3 70b instead of 3b

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 import pandas as pd
 from datetime import datetime
+from groq import Groq
+import os
 
-patient_data = pd.read_csv("vital signs data.csv")
+GROQ_API_KEY = os.environ.get("GROQ_API_KEY")
 
 
 def parse_patient_data(data):
+    """
+    Parse patient data from a dictionary into a human-readable report.
+
+    Parameters
+    ----------
+    data : dict
+        A dictionary containing the patient data.
+
+    Returns
+    -------
+    str
+        A human-readable report of the patient's vital signs and other information.
+    """
 
     # Extract values
     patient_id = data.get("Patient ID")
@@ -25,33 +40,185 @@ def parse_patient_data(data):
     map_value = data.get("Derived_MAP")
 
     # Parse timestamp to readable format
-    formatted_timestamp = datetime.strptime(timestamp, '%Y-%m-%d %H:%M:%S')
+    formatted_timestamp = datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S")
 
     # Generate report
     report = f"""
-    Patient Report:
-    ----------------
-    Patient ID: {patient_id}
-    Gender: {gender}
-    Age: {age} years
-    Weight: {weight} kg
-    Height: {height} m
-    BMI (Body Mass Index): {bmi}
+Rapport du Patient :
+--------------------
+ID du Patient : {patient_id}
+Sexe : {gender}
+Âge : {age} ans
+Poids : {weight} kg
+Taille : {height} m
+IMC (Indice de Masse Corporelle) : {bmi}
 
-    Vital Signs:
-    - Heart Rate: {heart_rate} bpm
-    - Respiratory Rate: {respiratory_rate} breaths/min
-    - Body Temperature: {body_temp} °C
-    - Oxygen Saturation: {oxygen_saturation} %
-    - Systolic Blood Pressure: {systolic_bp} mmHg
-    - Diastolic Blood Pressure: {diastolic_bp} mmHg
-    - Pulse Pressure: {pulse_pressure} mmHg
-    - Mean Arterial Pressure (MAP): {map_value} mmHg
-    - Heart Rate Variability (HRV): {hrv} ms
+Signes Vitaux :
+- Fréquence Cardiaque : {heart_rate} bpm
+- Fréquence Respiratoire : {respiratory_rate} respirations/min
+- Température Corporelle : {body_temp} °C
+- Saturation en Oxygène : {oxygen_saturation} %
+- Pression Artérielle Systolique : {systolic_bp} mmHg
+- Pression Artérielle Diastolique : {diastolic_bp} mmHg
+- Pression Différentielle : {pulse_pressure} mmHg
+- Pression Artérielle Moyenne (PAM) : {map_value} mmHg
+- Variabilité de la Fréquence Cardiaque (VFC) : {hrv} ms
 
-    Report Timestamp: {formatted_timestamp.strftime('%d %b %Y, %I:%M %p')}
-    ----------------
+Date et Heure du Rapport : {formatted_timestamp.strftime('%d %b %Y, %I:%M %p')}
+--------------------
     """
     return report.strip()
 
-    
+
+def generate_diagnosis(report):
+    """
+    Generate a diagnosis based on a patient report.
+
+    Parameters:
+        report (str): The patient report to base the diagnosis on.
+
+    Returns:
+        str: The generated diagnosis, including any additional tests or data that may be required.
+
+    Notes:
+        This function uses the Groq AI model to generate a diagnosis based on the provided patient report.
+        The diagnosis will be returned in the format "Diagnostic : <diagnostic>\nTests ou Données Supplémentaires Requises : <tests ou données>".
+    """
+    client = Groq(api_key="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")
+    # client = Groq(api_key=GROQ_API_KEY)
+    completion = client.chat.completions.create(
+        model="llama3-70b-8192",
+        messages=[
+            {
+                "role": "user",
+                "content": f"""Vous êtes un assistant médical expert. Sur la base des données fournies sur le patient, analysez les informations et fournissez un diagnostic préliminaire ou identifiez d'éventuelles préoccupations. Prenez en compte les éléments suivants :
+
+                            {report}
+
+                            Soyez concis mais précis. S'il n'y a pas suffisamment d'informations pour établir un diagnostic concluant, suggérez des tests ou des données supplémentaires qui permettraient de clarifier l'état du patient. Ne faites aucune supposition sur la santé du patient.
+                            Vous pouvez demander l'historique du patient si besoin.
+
+                            Votre réponse doit être formatée comme suit :
+
+                            Diagnostic : <diagnostic>
+                            Tests ou Données Supplémentaires Requises : <tests ou données>
+
+                            Réponse :""",
+            }
+        ],
+        temperature=0.1,  # trying a more deterministic approach
+        max_tokens=1024,
+        top_p=1,
+        stream=True,
+        stop=None,
+    )
+
+    diagnosis = ""
+
+    for chunk in completion:
+        diagnosis += chunk.choices[0].delta.content or ""
+
+    return diagnosis
+
+
+def generate_recommendations(report, diagnosis):
+    """
+    Generate recommendations based on patient report and diagnosis.
+
+    Parameters
+    ----------
+    report : str
+        The patient report.
+    diagnosis : str
+        The diagnosis of the patient.
+
+    Returns
+    -------
+    str
+        The recommendations for the patient, formatted as a string.
+
+    Notes
+    -----
+    This function uses the Groq API to generate recommendations based on the patient report and diagnosis.
+    The recommendations are generated using the LLaMA 3-70B-8192 model.
+    The function returns a string containing the recommendations, formatted as follows:
+
+        Recommandations : <recommandations>
+
+    where <recommandations> is the generated recommendations.
+    """
+    client = Groq(api_key="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")
+    # client = Groq(api_key=GROQ_API_KEY)
+    completion = client.chat.completions.create(
+        model="llama3-70b-8192",
+        messages=[
+            {
+                "role": "user",
+                "content": f"""Vous êtes un assistant médical expert en soins aux patients. Ci-dessous se trouvent un rapport de patient, un diagnostic et un contexte médical pertinent. Sur la base de ces informations, fournissez des recommandations claires et actionnables, telles que des traitements ou des ajustements de style de vie.
+
+                Rapport du Patient :
+                {report}
+
+                Diagnostic :
+                {diagnosis}
+
+                Sortie :
+                Recommandations : Fournissez des étapes détaillées à suivre, incluant des traitements, des changements de mode de vie ou des tests, adaptés à l'état du patient et au contexte fourni.
+
+                Concentrez-vous sur des recommandations spécifiques, actionnables et pertinentes pour les besoins du patient.
+
+                Votre réponse doit être formatée comme suit, n'ajoute rien d'autre que les recommandations :
+
+                Recommandations court terme : <recommandations court terme>
+                Recommandations moyen terme : <recommandations moyen terme>
+                Recommandations long terme : <recommandations long terme>
+
+                Réponse :""",
+            }
+        ],
+        temperature=0.1,  # trying a more deterministic approach
+        max_tokens=1024,
+        top_p=1,
+        stream=True,
+        stop=None,
+    )
+
+    recommendations = ""
+    for chunk in completion:
+        recommendations += chunk.choices[0].delta.content or ""
+
+    return recommendations
+
+
+def generate_report(data):
+    """
+    Generate a comprehensive patient report including diagnosis and recommendations.
+
+    Parameters
+    ----------
+    data : dict
+        A dictionary containing patient vital signs and other relevant data.
+
+    Returns
+    -------
+    str
+        A formatted string containing the patient report, diagnosis, and recommendations.
+
+    Notes
+    -----
+    This function consolidates the patient data into a report, generates a diagnosis based on the report,
+    and provides recommendations. The final output is a comprehensive string combining all these elements.
+    """
+    report = parse_patient_data(data)
+    diagnosis = generate_diagnosis(report)
+    recommendations = generate_recommendations(report, diagnosis)
+
+    report = report.strip() + "\n\n" + diagnosis.strip() + "\n\n" + recommendations
+
+    return report
+
+
+## for testing purposes
+# patient_data = pd.read_csv("vital signs data.csv")
+# vital_signs = patient_data.iloc[1].to_dict()
+#print(generate_report(vital_signs))
